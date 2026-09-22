e090663ae67718dd4a5dac6d1cf68c1db4c91344
az-la <hmgfan@proton.me>
2026-09-22 20:52:19 +0700

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://az-la.github.io',
+  base: '/world-of-fablea', // MUST match your repository name exactly
+});
