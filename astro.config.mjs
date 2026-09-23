40cf60744b3d52645addcf9fe21675dc392f55b3
az-la <hmgfan@proton.me>
2026-09-23 17:05:05 +0700

    Added trailing slash on base url, link adjustments

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config/
 export default defineConfig({
   site: 'https://az-la.github.io',
-  base: '/world-of-fablea', // MUST match your repository name exactly
+  base: '/world-of-fablea/', // MUST match your repository name exactly
   vite: {
     plugins: [tailwindcss()],
   },
